9e299ed2cf532507684083699181ffb4c9c96f88
Leif Hetland <lehetland1@gmail.com>
2016-04-12 19:58:09 -0500

    checking for neighbors with one test passing, next will apply rules again

--- csharp/GameOfLife/GameBoard.cs
+++ csharp/GameOfLife/GameBoard.cs
@@ -22,4 +22,55 @@ public class Gameboard
         }
     }
 
+    public void checkNeighbors()
+    {
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                int count = 0;
+
+                if (this.gameWorld[mod((x - 1),10), mod((y + 1),10)].IsAlive)
+                {
+                    count++;
+                }
+                if (this.gameWorld[mod((x), 10), mod((y + 1), 10)].IsAlive)
+                {
+                    count++;
+                }
+                if (this.gameWorld[mod((x + 1), 10), mod((y + 1), 10)].IsAlive)
+                {
+                    count++;
+                }
+
+                if (this.gameWorld[mod((x - 1), 10), mod((y), 10)].IsAlive)
+                {
+                    count++;
+                }
+                if (this.gameWorld[mod((x + 1), 10), mod((y), 10)].IsAlive)
+                {
+                    count++;
+                }
+
+                if (this.gameWorld[mod((x - 1), 10), mod((y - 1), 10)].IsAlive)
+                {
+                    count++;
+                }
+                if (this.gameWorld[mod((x), 10), mod((y - 1), 10)].IsAlive)
+                {
+                    count++;
+                }
+                if (this.gameWorld[mod((x + 1), 10), mod((y - 1), 10)].IsAlive)
+                {
+                    count++;
+                }
+                this.gameWorld[x, y].neighbors = count;
+            }
+        }
+    }
+int mod(int x, int m)
+{
+    return (x % m + m) % m;
+}
+
 }
--- csharp/GameOfLifeTests/GameboardTests.cs
+++ csharp/GameOfLifeTests/GameboardTests.cs
@@ -36,5 +36,41 @@ namespace GameofLifeTests
             Assert.AreEqual(actual, expected);
         }
 
+        [TestMethod]
+        public void CanICreateAGameBoardWithAnAliveCell()
+        {
+            //Arrange 
+            Gameboard gameboard = new Gameboard();
+
+            //Act
+            gameboard.gameWorld[5, 5].IsAlive = true;
+            var actual = gameboard.gameWorld[5, 5].IsAlive;
+            var expected = true;
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void CanICheckForNeighbors()
+        {
+            //Arrange 
+            Gameboard gameboard = new Gameboard();
+
+            //Act
+            gameboard.gameWorld[5, 5].IsAlive = true;
+            gameboard.gameWorld[5, 6].IsAlive = true;
+            gameboard.gameWorld[5, 7].IsAlive = true;
+            gameboard.checkNeighbors();
+            var expected = 2;
+            var actual = gameboard.gameWorld[5, 6].neighbors;
+
+            
+
+            //Assert
+            Assert.AreEqual(expected, actual );
+        }
+
+
     }
 }
