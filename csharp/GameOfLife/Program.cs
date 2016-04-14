2d50a097a5d6be98bb1ae172eddf3a4d4591b0a0
Leif Hetland <lehetland1@gmail.com>
2016-04-13 19:14:15 -0500

    currently have a glider working in the console

--- csharp/GameOfLife/Program.cs
+++ csharp/GameOfLife/Program.cs
@@ -11,7 +11,26 @@ namespace GameOfLife
     {
         static void Main(string[] args)
         {
-            
+            Gameboard gameboard = new Gameboard();
+
+            gameboard.gameWorld[5, 5].IsAlive = true;
+            gameboard.gameWorld[6, 5].IsAlive = true;
+            gameboard.gameWorld[7, 5].IsAlive = true;
+            gameboard.gameWorld[7, 4].IsAlive = true;
+            gameboard.gameWorld[6, 3].IsAlive = true;
+
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Clear();
+                Console.WriteLine(gameboard.PutInConsole());
+                gameboard.checkNeighbors();
+                Thread.Sleep(2000);
+                gameboard.nextGeneration();
+            }
+
+
         }
     }
 }
